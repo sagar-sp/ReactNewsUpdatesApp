f937202260a398bf35182e4e6a151fe27b3c6c3e
sagar-sp <70337142+sagar-sp@users.noreply.github.com>
2021-08-26 22:50:21 +0530

    prpos and states

--- project2/src/components/NewsItem.js
+++ project2/src/components/NewsItem.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+    
     render() {
+        let {title,description,imageurl,newsurl} = this.props;
         return (
-            <div>
-                   This Is News Item Component
+            <div className="my-3">
+                <div className="card" style={{width: "18rem"}}>
+                    <img className="card-img-top" src={imageurl} alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
+                </div>
             </div>
         )
     }
