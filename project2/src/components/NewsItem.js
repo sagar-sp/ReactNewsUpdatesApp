729b0c5c61ef0fabdcc5bd69a5cdb94d78c0b451
sagar-sp <70337142+sagar-sp@users.noreply.github.com>
2021-08-27 23:27:51 +0530

    api fetching and apply react router dom

--- project2/src/components/NewsItem.js
+++ project2/src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
         let {title,description,imageurl,newsUrl} = this.props;
         return (
             <div className="my-3">
-                <div className="card" style={{width: "18rem"}}>
+                <div className="card">
                     <img className="card-img-top" src={imageurl ? imageurl : "https://images.news18.com/ibnlive/uploads/2021/08/mumbai-162913082716x9.jpg"} alt="Card image cap" />
                     <div className="card-body">
                         <h5 className="card-title">{title.length > 45 ? title.slice(0,45)+"..." : title}</h5>
