bd952f3920bb6a197b24752ace3ef4deff354653
sagar-sp <70337142+sagar-sp@users.noreply.github.com>
2021-08-29 11:58:35 +0530

    Badge Adjustment

--- project2/src/components/NewsItem.js
+++ project2/src/components/NewsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
       <div className="my-3">
         <div className="card">
             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success" 
-            style={{zIndex : '1', left : '90'}}>
+            style={{zIndex : '1', left: "79%!important"}}>
                 {source}
                 <span className="visually-hidden">unread messages</span>
             </span>
